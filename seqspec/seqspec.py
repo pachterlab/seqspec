92b92c5a1f32a97a4ebbefe05bc86352244dc95f
Sina Booeshaghi <sbooeshaghi@gmail.com>
2022-01-28 16:20:52 -0800

    restructured code, can read and write yaml files, can print seq and get lens

--- seqspec/seqspec.py
+++ seqspec/seqspec.py
@@ -9,15 +9,15 @@ class Region(yaml.YAMLObject):
 
     def __init__(self,
                  name: str,
-                 seq_type: str,
-                 seq: Optional[str] = None,
-                 min_len: Optional[int] = None,
-                 max_len: Optional[int] = None,
+                 sequence_type: str,
+                 sequence: str = "",
+                 min_len: int = 1,
+                 max_len: int = 99,
                  onlist: Optional[str] = None,
                  join: Optional['Join'] = None) -> None:
         self.name = name
-        self.seq_type = seq_type
-        self.seq = seq
+        self.sequence_type = sequence_type
+        self.sequence = sequence
 
         self.min_len = min_len
         self.max_len = max_len
@@ -25,21 +25,39 @@ class Region(yaml.YAMLObject):
         self.onlist = onlist
         self.join = join
 
-    def printSeq(self):
+        if self.join:
+            self.min_len, self.max_len = self.get_len()
+            self.max_len += 1
+
+            self.sequence = self.get_sequence()
+
+    def get_sequence(self, s: str = "") -> str:
         if self.join:
             for n, r in self.join.regions.items():
-                r.printSeq()
+                s = r.get_sequence(s)
         else:
-            if self.seq:
-                return print(self.seq, end="")
-            elif self.seq is None:
-                return print("X", end="")
+            if self.sequence:
+                s += self.sequence
+                return s
+            elif self.sequence is None:
+                s += "X"
+                return s
+        return s
+
+    def get_len(self, min_l: int = 0, max_l: int = 0):
+        if self.join:
+            for n, r in self.join.regions.items():
+                min_l, max_l = r.get_len(min_l, max_l)
+        else:
+            min_l += self.min_len
+            max_l += self.max_len - 1
+        return (min_l, max_l)
 
     def __repr__(self) -> str:
         d = {
-            "seq_type": self.seq_type,
+            "sequence_type": self.sequence_type,
             "onlist": self.onlist,
-            "seq": self.seq,
+            "sequence": self.sequence,
             "min_len": self.min_len,
             "max_len": self.max_len,
             "join": self.join
@@ -93,177 +111,18 @@ class Assay(yaml.YAMLObject):
         }
         return f"{d}"
 
-    def toJSON(self):
+    def to_JSON(self):
         return json.dumps(self,
                           default=lambda o: o.__dict__,
                           sort_keys=False,
                           indent=4)
 
-    def toYAML(self, fname: str):
+    # note to_yaml is reserved for yaml.YAMLObject
+    def to_YAML(self, fname: str):
         with open(fname, 'w') as f:
             yaml.dump(self, f, sort_keys=False)
 
-    def printSpec(self):
+    def print_sequence(self):
         for name, region in self.assay_spec.items():
-            region.printSeq()
-
-
-# sci-rna-seq
-illumina_p5 = Region(name="illumina_p5",
-                     seq_type="fixed",
-                     onlist=None,
-                     seq="AATGATACGGCGACCACCGAGATCTACAC",
-                     min_len=29,
-                     max_len=30,
-                     join=None)
-
-i5 = Region(name="i5",
-            seq_type="onlist",
-            onlist="i5_onlist.txt",
-            seq="NNNNNNNN",
-            min_len=8,
-            max_len=9,
-            join=None)
-
-truseq_read_1_adapter = Region(name="truseq_read_1_adapter",
-                               seq_type="fixed",
-                               onlist=None,
-                               seq="ACACTCTTTCCCTACACGACGCTCTTCCGATCT",
-                               min_len=33,
-                               max_len=34,
-                               join=None)
-
-umi = Region(name="umi",
-             seq_type="random",
-             onlist=None,
-             seq="NNNNNNNN",
-             min_len=8,
-             max_len=9,
-             join=None)
-
-cell_bc = Region(name="cell_bc",
-                 seq_type="onlist",
-                 onlist="barcodes.txt",
-                 seq="NNNNNNNNNNNNNN",
-                 min_len=14,
-                 max_len=15)
-
-read_1 = Region(name="read_1",
-                seq_type="joined",
-                seq=None,
-                min_len=None,
-                max_len=None,
-                join=Join(how="union",
-                          order=["umi", "cell_bc"],
-                          regions={
-                              "umi": umi,
-                              "cell_bc": cell_bc
-                          }))
-
-poly_T = Region(name="poly_T",
-                seq_type="random",
-                onlist=None,
-                seq=None,
-                min_len=1,
-                max_len=99,
-                join=None)
-
-cdna = Region(name="cdna",
-              seq_type="random",
-              onlist=None,
-              seq=None,
-              min_len=1,
-              max_len=99,
-              join=None)
-
-read_2 = Region(name="read_2",
-                seq_type="joined",
-                onlist=None,
-                min_len=None,
-                max_len=None,
-                join=Join(how="union", order=["cdna"], regions={"cdna": cdna}))
-
-ME = Region(name="ME",
-            seq_type="fixed",
-            seq="CTGTCTCTTATACACATCT",
-            min_len=19,
-            max_len=20,
-            onlist=None,
-            join=None)
-s7 = Region(name="s7",
-            seq_type="fixed",
-            seq="CCGAGCCCACGAGAC",
-            min_len=15,
-            max_len=16,
-            onlist=None,
-            join=None)
-
-i7_primer = Region(name="i7_primer",
-                   seq_type="joined",
-                   seq=None,
-                   min_len=None,
-                   max_len=None,
-                   onlist=None,
-                   join=Join(how="union",
-                             order=["ME", "s7"],
-                             regions={
-                                 "ME": ME,
-                                 "s7": s7
-                             }))
-
-i7 = Region(name="i7",
-            seq_type="fixed",
-            onlist="i7_onlist.txt",
-            seq="NNNNNNNNNN",
-            min_len=10,
-            max_len=11)
-
-illumina_p7 = Region(name="illumina_p7",
-                     seq_type="fixed",
-                     onlist=None,
-                     seq="ATCTCGTATGCCGTCTTCTGCTTG",
-                     min_len=24,
-                     max_len=25)
-
-RNA = Region(name="RNA",
-             seq_type="joined",
-             join=Join(
-                 order=[
-                     "illumina_p5",
-                     "i5",
-                     "truseq_read_1_adapter",
-                     "read_1",
-                     "read_2",
-                     "i7_primer",
-                     "i7",
-                     "illumina_p7",
-                 ],
-                 how="union",
-                 regions={
-                     "illumina_p5": illumina_p5,
-                     "i5": i5,
-                     "truseq_read_1_adapter": truseq_read_1_adapter,
-                     "read_1": read_1,
-                     "read_2": read_2,
-                     "i7_primer": i7_primer,
-                     "i7": i7,
-                     "illumina_p7": illumina_p7
-                 },
-             ))
-
-assay = Assay(
-    name="sci-RNA-seq",
-    doi="https://doi.org/10.1126/science.aam8940",
-    description="combinatorial single-cell RNA-seq",
-    modalities=["RNA"],
-    lib_struct=  # noqa
-    "https://teichlab.github.io/scg_lib_structs/methods_html/sci-RNA-seq.html",
-    assay_spec={"RNA": RNA})
-
-# i7_primer.printSeq()
-# assay.printSpec()
-
-assay.toYAML("spec.yaml")
-with open("spec.yaml", 'r') as stream:
-    data_loaded: Assay = yaml.load(stream, Loader=yaml.Loader)
-data_loaded.printSpec()
+            print(region.get_sequence(), end="")
+        print("\n", end="")
