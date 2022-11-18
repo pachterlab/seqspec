e1eb1519e4544cc681a4202e6f61efe8995350ec
Sina Booeshaghi <sbooeshaghi@gmail.com>
2022-11-18 10:26:48 -0800

    defining some gold standard assays, cleaning up the Illumina template specs, adding predefined naming conventions to the readme, begin development of seqspec diff to diff to specs

--- seqspec/seqspec.py
+++
@@ -1,6 +0,0 @@
-from .Region import Region
-
-
-def align_regions(r1: Region, r2: Region):
-    # compute score for any two regions (atomic)
-    pass
---
+++ seqspec/seqspec_diff.py
@@ -0,0 +1,47 @@
+from .Region import Region
+from seqspec.Assay import Assay
+import yaml
+
+
+def setup_diff_args(parser):
+    parser_diff = parser.add_parser(
+        "diff",
+        description="diff two seqspecs",
+        help="diff two seqspecs",
+    )
+    parser_diff.add_argument("yamlA", help="Sequencing specification yaml file A")
+    parser_diff.add_argument("yamlB", help="Sequencing specification yaml file B")
+    parser_diff.add_argument(
+        "-o",
+        metavar="OUT",
+        help=("Path to output file"),
+        type=str,
+        default=None,
+    )
+    return parser_diff
+
+
+def validate_diff_args(parser, args):
+    # if everything is valid the run_diff
+    A_fn = args.yamlA
+    B_fn = args.yamlB
+    o = args.o
+    with open(A_fn, "r") as stream:
+        A: Assay = yaml.load(stream, Loader=yaml.Loader)
+    with open(B_fn, "r") as stream:
+        B: Assay = yaml.load(stream, Loader=yaml.Loader)
+
+    # load in two specs
+    run_diff(A, B)
+
+
+def run_diff(A: Assay, B: Assay):
+    # What does it mean to diff two assays?
+    # Only compare on modalities?
+    # itx: pull out regions that have the same name?
+    # itx:
+    pass
+
+
+def diff_regions(R1: Region, R2: Region):
+    pass
