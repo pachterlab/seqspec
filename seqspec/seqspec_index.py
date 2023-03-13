921f86e25e6be7f25ad62e8cf8bd7269128fc3a6
Sina Booeshaghi <sbooeshaghi@gmail.com>
2023-03-13 03:23:48 -0700

    index for various tools

--- seqspec/seqspec_index.py
+++ seqspec/seqspec_index.py
@@ -32,6 +32,15 @@ def setup_index_args(parser):
         type=str,
         default=None,
     )
+
+    parser_index.add_argument(
+        "-t",
+        metavar="TOOL",
+        help=("Tool"),
+        type=str,
+        default="tab",
+    )
+
     parser_index.add_argument(
         "--rev", help="Returns 3'->5' region order", action="store_true"
     )
@@ -44,22 +53,32 @@ def validate_index_args(parser, args):
     fn = args.yaml
     m = args.m
     r = args.r
+    t = args.t
     o = args.o
     rev = args.rev
 
     # load spec
     spec = load_spec(fn)
+    rgns = r.split(",")
 
-    index = run_index(spec, m, r, rev=rev)
+    indices = []
+    for r in rgns:
+        index = run_index(spec, m, r, rev=rev)
+        indices.append({r: index})
 
+    FORMAT = {
+        "kb": format_kallisto_bus,
+        "starsolo": format_starsolo,
+        "tab": format_tab,
+        "simpleaf": format_simpleaf,
+        "zumis": format_zumis,
+    }
     # post processing
     if o:
         with open(o, "w") as f:
-            for k, v in index.items():
-                print(f"{v}\t{k[0]}\t{k[1]}", file=f)
+            print(FORMAT[t](indices), file=f)
     else:
-        for k, v in index.items():
-            print(f"{v}\t{k[0]}\t{k[1]}")
+        print(FORMAT[t](indices))
 
 
 def run_index_by_type(
@@ -99,3 +118,84 @@ def run_index(spec, modality, region_id, rev=False) -> Dict[Tuple[int, int], str
         index[c] = t
 
     return index
+
+
+def format_kallisto_bus(indices):
+    bcs = []
+    umi = []
+    cdna = []
+    for idx, region in enumerate(indices):
+        for rgn, index in region.items():
+            for k, v in index.items():
+                if v == "barcode":
+                    bcs.append(f"{idx},{k[0]},{k[1]}")
+                elif v == "umi":
+                    umi.append(f"{idx},{k[0]},{k[1]}")
+                elif v == "cDNA":
+                    cdna.append(f"{idx},{k[0]},{k[1]}")
+
+    x = "&".join(bcs) + ":" + "&".join(umi) + ":" + "&".join(cdna)
+    return x
+
+
+def format_tab(indices):
+    x = ""
+    for idx, region in enumerate(indices):
+        for rgn, index in region.items():
+            for k, v in index.items():
+                x += f"{rgn}\t{v}\t{k[0]}\t{k[1]}\n"
+
+    return x[:-1]
+
+
+def format_starsolo(indices):
+    bcs = []
+    umi = []
+    cdna = []
+    for idx, region in enumerate(indices):
+        for rgn, index in region.items():
+            for k, v in index.items():
+                if v == "barcode":
+                    bcs.append(f"--soloCBstart {k[0] + 1} --soloCBlen {k[1]}")
+                elif v == "umi":
+                    umi.append(f"--soloUMIstart {k[0] + 1} --soloUMIlen {k[1] - k[0]}")
+                elif v == "cDNA":
+                    cdna.append(f"{k[0]},{k[1]}")
+    x = f"--soloType CB_UMI_Simple {bcs[0]} {umi[0]}"
+    return x
+
+
+def format_simpleaf(indices):
+    x = ""
+    xl = []
+    for idx, region in enumerate(indices):
+        fn = idx
+        x = f"{fn+1}{{"
+        for rgn, index in region.items():
+            for k, v in index.items():
+                if v == "barcode":
+                    x += f"b[{k[1]-k[0]}]"
+                elif v == "umi":
+                    x += f"u[{k[1]-k[0]}]"
+                elif v == "cDNA":
+                    x += f"r[{k[1] - k[0]}]"
+            x += "x:}"
+        xl.append(x)
+    return "".join(xl)
+
+
+def format_zumis(indices):
+    xl = []
+    for idx, region in enumerate(indices):
+        x = ""
+        for rgn, index in region.items():
+            for k, v in index.items():
+                if v == "barcode":
+                    x += f"- BCS({k[0] + 1}-{k[1]})\n"
+                elif v == "umi":
+                    x += f"- UMI({k[0] + 1}-{k[1]})\n"
+                elif v == "cDNA":
+                    x += f"- cDNA({k[0] + 1}-{k[1]})\n"
+        xl.append(x)
+
+    return "\n".join(xl)[:-1]
