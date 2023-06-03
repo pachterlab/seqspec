fd222cafdbf4afb32977eae335a5f4d17b250c3a
Sina Booeshaghi <sbooeshaghi@gmail.com>
2023-06-02 22:40:44 -0700

    added seqspec find by region type, added seqspec check functionality to see if the onlist file exists in the path relative to the spec

--- seqspec/seqspec_check.py
+++ seqspec/seqspec_check.py
@@ -34,15 +34,36 @@ def validate_check_args(parser, args):
 
     spec = load_spec(spec_fn)
 
-    return run_check(schema, spec)  # , o)
+    return run_check(schema, spec, spec_fn)  # , o)
 
 
-def run_check(schema, spec):
+def run_check(schema, spec, spec_fn):
     v = Draft4Validator(schema)
-    idx = 0
+    idx = 1
     for idx, error in enumerate(v.iter_errors(spec.to_dict()), 1):
         print(
             f"[error {idx}] {error.message} in spec[{']['.join(repr(index) for index in error.path)}]"
         )
+    # get all of the onlist files in the spec and check that they exist relative to the path of the spec
+    modes = spec.modalities
+    olrgns = []
+    for m in modes:
+        olrgns += [i.onlist for i in spec.get_modality(m).get_onlist_regions()]
+
+    # check paths relative to spec_fn
+    for ol in olrgns:
+        if ol.filename[:-3] == ".gz":
+            check = path.join(path.dirname(spec_fn), ol.filename[:-3])
+            if not path.exists(check):
+                print(f"[error {idx}] {ol.filename[:-3]} does not exist")
+                idx += 1
+        else:
+            check = path.join(path.dirname(spec_fn), ol.filename)
+            check_gz = path.join(path.dirname(spec_fn), ol.filename + ".gz")
+            if not path.exists(check) and not path.exists(check_gz):
+                print(f"[error {idx}] {ol.filename} does not exist")
+                idx += 1
+
+    # TODO add option to check md5sum
 
     return idx
